14f9729c329a484525f74a9613e8f427836a0eaa
CIA <zhixing.yu@mail.utoronto.ca>
2025-03-06 13:01:28 -0500

    Adding docs for io saveFilePublic

--- my-next-backend/src/utils/io.js
+++ my-next-backend/src/utils/io.js
@@ -2,9 +2,13 @@ import { mkdir, writeFile } from "fs/promises";
 import { join, extname } from "path";
 
 export async function saveFilePublic(pathUnderPublic, file) {
+    // Given a path (under the public folder), save the file.
+    // Return the saved filename (the time of processing in nanoseconds).
+
     const filename = Date.now().toString() + extname(file.name);
     const saveDir = join(process.cwd(), 'public', pathUnderPublic);
 
+    // Ensure folder exist
     await mkdir(saveDir, { recursive: true });
 
     await writeFile(
