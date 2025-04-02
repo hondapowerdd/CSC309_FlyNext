3917bf8c408d1468468fd23d31e5888f7acffaed
CIA <zhixing.yu@mail.utoronto.ca>
2025-04-02 12:29:26 -0400

    Fix issue

--- flynext/app/page.tsx
+++ flynext/app/page.tsx
@@ -6,7 +6,7 @@ export default function HomePage(): JSX.Element {
     return (
         <div>
             <div className="relative">
-                <img src={""} alt="Hero" className="w-full h-96 object-cover" />
+                <img alt="Hero" className="w-full h-96 object-cover" />
                 <div className="absolute inset-0 bg-black bg-opacity-40 flex flex-col justify-center items-center text-white">
                     <h1 className="text-3xl font-bold">flynext</h1>
                     <p className="mt-2">good</p>
